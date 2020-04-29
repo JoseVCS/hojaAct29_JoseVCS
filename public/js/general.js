ec91a6119ea8cd06ef5ca06c8af04e90f2c716d9
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-04-29 16:10:18 +0200

    Creada tabla para mostrar datos usando Handlebars * He creado una tabla usando Handlebars para mostrar los resultados * He respetado los estilos que habían.

--- public/js/general.js
+++ public/js/general.js
@@ -24,16 +24,8 @@ function muestraResultados(categoria, resp){
    let resultado = document.getElementById("resultado");
    let totalRes = document.getElementById("totalRes");
    let txtTotal = "Total de API's encontradas con relación a la categoría elegida : <span class='etiqueta'>" + resp.length + "</span>";
-   let txt = "";
-   resp.forEach(element => {
-      /* TODO: Mostrar datos en una tabla. Usar plantilla handlebars es recomendable pero opcional */
-      txt += "<p>";
-      txt += "<span class='etiqueta'>Nombre</span> " + element.API;
-      txt += " <span class='etiqueta'>Descripción</span> " + element.Description;
-      txt += "</p>";
-   });
    totalRes.innerHTML = txtTotal;
-   resultado.innerHTML = txt;
+   resultado.innerHTML = Handlebars.templates['partials_tabla.hbs']({resp});
    resultado.className = "visible";
 }
 
